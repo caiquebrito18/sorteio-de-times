12f41d34addc86d0dfabd670e39a365b320b7ee8
Caique Brito <brs.caique@gmail.com>
2022-06-19 19:13:06 -0300

    Adicionando componente de header e menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
